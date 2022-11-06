e087a3130aa47c54698741efd1b34cb82c0b7701
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-11-06 20:43:06 +0100

    création fichier navigation.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,7 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector(".close");
-const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
