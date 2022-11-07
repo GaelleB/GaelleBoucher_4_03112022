00e6be18679339a17fc78f30fa46c79be9da188f
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-11-07 13:17:17 +0100

    formulaire

--- src/js/formulaire.js
+++ src/js/formulaire.js
@@ -18,9 +18,36 @@ form.addEventListener('submit', e => {
     validate();
 });
 
+// firstname.addEventListener('change', () => {
+//     textInput(firstname, regexText);
+// });
+
+// lastname.addEventListener('change', () => {
+//     textInput(lastname, regexText);
+// });
+
+function textInput(){
+    if (regexText.test(firstname.value) === false) {
+        alert("Votre nom doit comporter au minimum 2 lettres et ne doit pas comporter de chiffres ou de caractères spéciaux autre que -")
+    }
+    else if (regexText.test(lastname.value) === false) {
+        alert("Votre prenom doit comporter au minimum 2 lettres et ne doit pas comporter de chiffres ou de caractères spéciaux autre que -")
+    }
+    else if (regexEmail.test(email.value) === false) {
+        alert("Ce format d'email n'est pas valide")
+    } 
+    else if (regexDate.test(birthday.value) === false) {
+        alert("Ce format de date n'est pas valide")
+    } 
+}
+
 const validate = () => {
     const firstnameValue = firstname.value.trim();
     const lastnameValue = lastname.value.trim();
     const emailValue = email.value.trim();
     const birthdayValue = birthday.value.trim();
-}
+};
+
+const regexText = /^[a-zA-Z-\s]{2,}$/;
+const regexEmail = /^([a-zA-Z0-9.-_]+)@((?:[a-zA-Z0-9.-_]+.)+)([a-zA-Z]{2,4})/;
+const regexDate = /^(\d{4})-(0[1-9]|1[0-2]|[1-9])-([1-9]|0[1-9]|[1-2]\d|3[0-1])$/;
