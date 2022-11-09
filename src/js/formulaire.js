781095040bc29790e3d93f76c33177ff1b481472
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-11-09 12:03:14 +0100

    formulaire input (firstname, lastname, email, birthdate

--- src/js/formulaire.js
+++ src/js/formulaire.js
@@ -1,8 +1,9 @@
+// DOM Elements
 const form = document.getElementById('form');
 const firstname = document.getElementById('first');
 const lastname = document.getElementById('last');
 const email = document.getElementById('email');
-const birthday = document.getElementById('birthdate');
+const birthdate = document.getElementById('birthdate');
 const quantity = document.getElementById('quantity');
 const location1 = document.getElementById('location1');
 const location2 = document.getElementById('location2');
@@ -13,41 +14,57 @@ const location6 = document.getElementById('location6');
 const userCondition = document.getElementById('checkbox1');
 const nextEvent = document.getElementById('checkbox2');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    validate();
-});
-
-// firstname.addEventListener('change', () => {
-//     textInput(firstname, regexText);
-// });
-
-// lastname.addEventListener('change', () => {
-//     textInput(lastname, regexText);
-// });
-
-function textInput(){
-    if (regexText.test(firstname.value) === false) {
-        alert("Votre nom doit comporter au minimum 2 lettres et ne doit pas comporter de chiffres ou de caractères spéciaux autre que -")
-    }
-    else if (regexText.test(lastname.value) === false) {
-        alert("Votre prenom doit comporter au minimum 2 lettres et ne doit pas comporter de chiffres ou de caractères spéciaux autre que -")
-    }
-    else if (regexEmail.test(email.value) === false) {
-        alert("Ce format d'email n'est pas valide")
-    } 
-    else if (regexDate.test(birthday.value) === false) {
-        alert("Ce format de date n'est pas valide")
-    } 
-}
-
-const validate = () => {
-    const firstnameValue = firstname.value.trim();
-    const lastnameValue = lastname.value.trim();
-    const emailValue = email.value.trim();
-    const birthdayValue = birthday.value.trim();
-};
-
+// Regex
 const regexText = /^[a-zA-Z-\s]{2,}$/;
 const regexEmail = /^([a-zA-Z0-9.-_]+)@((?:[a-zA-Z0-9.-_]+.)+)([a-zA-Z]{2,4})/;
-const regexDate = /^(\d{4})-(0[1-9]|1[0-2]|[1-9])-([1-9]|0[1-9]|[1-2]\d|3[0-1])$/;
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkFirstname()
+    checkLastname()
+    checkEmail()
+    checkDate()
+
+    const isFormValid = form.checkValidity()
+    if(isFormValid){
+        console.log()
+    }
+    console.log(isFormValid)
+});
+
+const checkFirstname = () => {
+    const firstnameValue = firstname.value.trim();
+    firstname.setCustomValidity("")
+    if (regexText.test(firstnameValue) === false) {
+        firstname.setCustomValidity("Votre prénom doit comporter au minimum 2 lettres et ne doit pas comporter de chiffres ou de caractères spéciaux autre que -")
+    }
+    firstname.reportValidity()
+};
+firstname.addEventListener('change', checkFirstname);
+
+const checkLastname = () => {
+    const lastnameValue = lastname.value.trim();
+    lastname.setCustomValidity("")
+    if (regexText.test(lastnameValue) === false) {
+        lastname.setCustomValidity("Votre nom doit comporter au minimum 2 lettres et ne doit pas comporter de chiffres ou de caractères spéciaux autre que -")
+    }
+    lastname.reportValidity()
+};
+lastname.addEventListener('change', checkLastname);
+
+const checkEmail = () => {
+    const emailValue = email.value.trim();
+    email.setCustomValidity("")
+    if (regexEmail.test(emailValue) === false) {
+        email.setCustomValidity("Saisissez une adresse mail valide")
+    }
+    email.reportValidity()
+};
+email.addEventListener('change', checkEmail);
+
+const checkDate = () => {
+    birthdate.setCustomValidity("")
+    birthdate.reportValidity()
+};
+birthdate.addEventListener('change', checkDate);
