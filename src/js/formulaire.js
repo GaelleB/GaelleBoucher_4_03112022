d615a3a7fa42ec9459a144a80dec823b480aee85
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-11-25 14:09:12 +0100

    fonction birthdate

--- src/js/formulaire.js
+++ src/js/formulaire.js
@@ -12,21 +12,23 @@ const nextEvent = document.getElementById('checkbox2');
 // Regex
 const regexText = /^[a-zA-Z-\s]{2,}$/;
 const regexEmail = /^([a-zA-Z0-9.-_]+)@((?:[a-zA-Z0-9.-_]+.)+)([a-zA-Z]{2,4})/;
-const regexDate =  /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+const regexDate = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
 
+// Ecoute du formulaire de soumission
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     checkFirstname();
     checkLastname();
     checkEmail();
-    checkDate();
+    // checkDate();
     checkQuantityTournoi();
     checkLocation();
     checkUserConditions();
     validate();
 });
 
+// Fonction gestion des attributs data-error
 const handleAttributes = (input) => {
     const inputParent = input.parentElement;
     const isInputValid = input.checkValidity();
@@ -37,6 +39,7 @@ const handleAttributes = (input) => {
     }
 };
 
+// Fonction gestion des attributs data-error des boutons radio
 const handleAttributesRadio = (inputs) => {
     inputs.forEach(input => {
         const inputParent = input.parentElement;
@@ -49,6 +52,7 @@ const handleAttributesRadio = (inputs) => {
     });
 };
 
+//  Fonction prénom
 const checkFirstname = () => {
     const firstnameValue = firstname.value.trim();
     firstname.setCustomValidity("");
@@ -59,6 +63,7 @@ const checkFirstname = () => {
 };
 firstname.addEventListener('change', checkFirstname);
 
+//  Fonction nom
 const checkLastname = () => {
     const lastnameValue = lastname.value.trim();
     lastname.setCustomValidity("");
@@ -69,6 +74,7 @@ const checkLastname = () => {
 };
 lastname.addEventListener('change', checkLastname);
 
+//  Fonction email
 const checkEmail = () => {
     const emailValue = email.value.trim();
     email.setCustomValidity("");
@@ -79,16 +85,18 @@ const checkEmail = () => {
 };
 email.addEventListener('change', checkEmail);
 
-const checkDate = () => {
-    const birthdateValue = birthdate.value.trim();
-    birthdate.setCustomValidity("");
-    if (regexDate.test(birthdateValue) === false) {
-        birthdate.setCustomValidity("Veuillez entrer une date");
-    }
-    handleAttributes(birthdate);
-};
-birthdate.addEventListener('change', checkDate);
+//  Fonction date de naissance
+// const checkDate = () => {
+//     const birthdateValue = birthdate.value;
+//     birthdate.setCustomValidity("");
+//     if (regexDate.test(birthdateValue) === false) {
+//         birthdate.setCustomValidity("Veuillez entrer une date");
+//     }
+//     handleAttributes(birthdate);
+// };
+// birthdate.addEventListener('change', checkDate);
 
+//  Fonction quantité des tournois
 const checkQuantityTournoi = () => {
     const quantityValue = parseInt(quantityTournoi.value, 10);
     quantityTournoi.setCustomValidity("");
@@ -99,6 +107,7 @@ const checkQuantityTournoi = () => {
 };
 quantityTournoi.addEventListener('change', checkQuantityTournoi);
 
+//  Fonction boutons radio (ville à sélectionner)
 const checkLocation = () => {
     const isAnythingChecked = document.querySelector('input[type="radio"]:checked');
     if (isAnythingChecked) {
@@ -116,6 +125,7 @@ locationInput.forEach(location => {
     location.addEventListener('change', checkLocation);
 });
 
+//  Fonction condition d'utilisateur
 const checkUserConditions = () => {
     userCondition.setCustomValidity("");
     if (userCondition.checked === false) {
@@ -125,6 +135,7 @@ const checkUserConditions = () => {
 };
 userCondition.addEventListener('change', checkUserConditions);
 
+//  Fonction newsletter
 const checkNextEvent = () => {
     if (nextEvent.checked === true) {
         return true;
@@ -134,6 +145,7 @@ const checkNextEvent = () => {
 };
 nextEvent.addEventListener('change', checkNextEvent);
 
+//  Fonction validate
 const validate = () => {
     const isFormValid = form.checkValidity();
     if (isFormValid) {
@@ -152,6 +164,7 @@ const validate = () => {
     }
 };
 
+//  Fonction pour afficher la fenêtre de confirmation
 const launchConfirmationModal = () => {
 	modalConfirmation.style.display = "block";
 	console.log('Formulaire envoyé');
